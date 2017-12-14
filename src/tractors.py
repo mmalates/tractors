606e6aedc134a9c7739077220441094a43fcd361
mmalates <malatesta.mike@gmail.com>
2017-12-14 13:09:05 -0800

    removed pkl

--- src/tractors.py
+++ src/tractors.py
@@ -2,6 +2,10 @@ from ds_utils import Predictor
 import pandas as pd
 import numpy as np
 
+"""TODO:
+    -fix data_to_predict processing
+"""
+
 
 class Tractors(Predictor):
 
@@ -20,12 +24,10 @@ class Tractors(Predictor):
                 self.test['YearMade']
             self.test.loc[self.test['YearMade'] == 1000, 'YearMade'] = 2000
         data.loc[data['sale_age'] > 500, 'sale_age'] = np.nan
-        data = self.predict_missing_values(data, 'RandomForestRegressor', ['sale_age'], [
-                                           'MachineID', 'ModelID', 'YearMade'], **forest_params)
-        data = self.predict_missing_values(data, 'RandomForestRegressor', ['MachineHoursCurrentMeter'], [
-                                           'MachineID', 'ModelID', 'YearMade', 'sale_age'], **forest_params)
-        data = self.predict_missing_values(data, 'RandomForestClassifier', ['UsageBand'], [
-                                           'MachineID', 'ModelID', 'YearMade', 'sale_age'], **forest_params)
+        data = self.predict_missing_values(
+            data, ['sale_age'], ['MachineID', 'ModelID', 'YearMade'])
+        data = self.predict_missing_values(data, ['MachineHoursCurrentMeter'], [
+                                           'MachineID', 'ModelID', 'YearMade', 'sale_age'])
         dummies = [u'auctioneerID', u'YearMade',
                    u'ProductSize',
                    u'state', u'ProductGroup',
@@ -35,8 +37,8 @@ class Tractors(Predictor):
                    u'Pushblock', u'Ripper', u'Scarifier', u'Tip_Control', u'Tire_Size',
                    u'Coupler', u'Coupler_System', u'Grouser_Tracks', u'Hydraulics_Flow',
                    u'Track_Type',  u'Stick_Length', u'Thumb',
-                   u'Pattern_Changer', u'Grouser_Type', u'Backhoe_Mounting', u'Blade_Type', 'UsageBand',
-                   u'Travel_Controls', u'Differential_Type', u'Steering_Controls', u'fiSecondaryDesc', u'fiModelSeries']
+                   u'Pattern_Changer', u'Grouser_Type', u'Backhoe_Mounting', u'Blade_Type',
+                   u'Travel_Controls', u'Differential_Type', u'Steering_Controls', u'fiModelSeries']
         data = self.dummify(data, dummies)
         for column in data.columns:
             if 'None' in column:
@@ -52,60 +54,72 @@ class Tractors(Predictor):
         return data
 
 
-print 'loading data'
-train = pd.read_csv('../data/train.csv', low_memory=False)
-data_to_predict = pd.read_csv('../data/test.csv', low_memory=False)
+if __name__ == '__main__':
+    # load the training data and data to predict
+    print 'loading data'
+    train = pd.read_csv('../data/train.csv', low_memory=False)
+    data_to_predict = pd.read_csv('../data/test.csv', low_memory=False)
 
-# initialize Tractors instance
-tractors = Tractors(
-    data=train, data_to_predict=data_to_predict, target='SalePrice')
+    # initialize Tractors instance
+    tractors = Tractors(
+        data=train, data_to_predict=data_to_predict, target='SalePrice')
 
-print 'processing data'
-tractors.data = tractors.processing(tractors.data)
-tractors.data_to_predict = tractors.processing(tractors.data_to_predict)
+    # process the training data and data to predict
+    print 'processing data'
+    tractors.data = tractors.processing(tractors.data)
+    tractors.data_to_predict = tractors.processing(tractors.data_to_predict)
 
-print 'splitting data'
-tractors.split(test_size=0.1, random_state=42)
+    # train test split
+    print 'splitting data'
+    tractors.split(test_size=0.8, random_state=42)
 
-# get a baseline to compare our results with
-tractors.mean_baseline()
+    # get a baseline to compare our results with
+    tractors.mean_baseline()
 
-# set features to train on
-drop_features = ['SalesID', 'fiBaseModel', 'SalePrice', 'MachineID', 'datasource',
-                 'saledate', 'fiModelDesc', 'fiModelDescriptor', 'fiProductClassDesc', 'ProductGroupDesc', 'Undercarriage_Pad_Width', 'Blade_Width', 'Blade_Extension', 'ModelID']
-tractors.set_features(tractors.train.drop(drop_features, axis=1).columns)
+    # set features to train on
+    drop_features = ['SalesID', 'fiBaseModel', 'SalePrice', 'MachineID', 'datasource',
+                     'saledate', 'fiModelDesc', 'fiModelDescriptor', 'fiProductClassDesc', 'ProductGroupDesc', 'Undercarriage_Pad_Width', 'Blade_Width', 'Blade_Extension', 'ModelID',  u'fiSecondaryDesc', 'UsageBand']
+    tractors.set_features(tractors.train.drop(drop_features, axis=1).columns)
 
-# perform features selection with lassoCV
-print 'selecting features'
-tractors.select_features()
+    # perform features selection with lassoCV
+    print 'selecting features'
+    tractors.select_features()
 
-# grid search model hyperparameter
-print 'tuning hyperparameters'
-model_name = 'RandomForestRegressor'
-param_grid = {'n_estimators': [50],
-              'max_depth': [50, None],
-              'min_samples_leaf': [2, 6, 10],
-              'bootstrap': [True, False]
-              'n_jobs': [-1]}
-tractors.grid_search(model_name, param_grid)
-print 'best train RMSE: {}'.format(tractors.train_score_)
+    # grid search model hyperparameter
+    print 'tuning hyperparameters'
+    model_name = 'RandomForestRegressor'
+    param_grid = {'n_estimators': [10],
+                  'max_features': ['auto', 0.6, 0.2],
+                  'max_depth': [None],
+                  'min_samples_leaf': [2],
+                  'bootstrap': [True],
+                  'n_jobs': [-1],
+                  'verbose': [0]}
+    tractors.grid_search(model_name, param_grid)
+    print 'best train RMSE: {}'.format(np.sqrt(abs(tractors.train_score_)))
 
-# increase the number of estimators for the final test
-tractors.best_params_['n_estimators'] = 1000
+    # score the best model
+    print 'testing the performance on unseen data'
+    tractors.score(model_name, **tractors.best_params_)
+    print 'test RMSE: {}'.format(tractors.test_score_)
 
-# score the best model
-print 'testing the performance on unseen data'
-tractors.score(model_name, **tractors.best_params_)
-print 'test RMSE: {}'.format(tractors.test_score_)
+    # increase the number of estimators for the final fit
+    tractors.best_params_['n_estimators'] = 100
 
+    # fit the best model on the whole dataset
+    print 'fitting final model'
+    tractors.fit(model_name, **tractors.best_params_)
+    tractors.best_params_
 
-# fit the best model on the whole dataset
-print 'fitting final model'
-tractors.fit(model_name, **tractors.best_params_)
+    # pickle the model
+    print 'pickling model'
+    tractors.pickle_model('RandomForestRegressor.pkl')
 
-# pickle the model
-tractors.pickle_model('RandomForestRegressor.pkl')
-
-# predict new data
-tractors.predict()
-tractors.data_to_predict['Predicted_SalePrice'] = tractors.predictions
+    # predict new data
+    print 'making predictions'
+    tractors.predict()
+    tractors.data_to_predict['Predicted_SalePrice'] = tractors.predictions
+    print tractors.predictions
+    tractors.data.shape
+    tractors.train.head()
+    tractors.data_to_predict[].head()
